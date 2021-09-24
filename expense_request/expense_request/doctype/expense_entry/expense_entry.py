fc2050d67e8d2d65ea2d89d4ebdae94e4f2e07d7
Sujith Reddy <83070636+MSRSK@users.noreply.github.com>
2021-09-24 19:01:33 +0530

    Closing Balance Introduced (#6)

--- expense_request/expense_request/doctype/expense_entry/expense_entry.py
+++ expense_request/expense_request/doctype/expense_entry/expense_entry.py
@@ -3,8 +3,22 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnext.accounts.report.account_balance.account_balance import get_data
+from erpnext.accounts.utils import get_balance_on
+
 
 class ExpenseEntry(Document):
-	pass
+    pass
+
+@frappe.whitelist()
+def get_closing_balance(mode_of_payment):
+    account = frappe.db.get_value('Mode of Payment Account', {
+                                  'parent': mode_of_payment}, 'default_account')
+    if account:
+        balance = get_balance_on(account)
+        return balance
+    else:
+        frappe.msgprint("<b>No default account</b> is associated with this mode of payment")
+        return False
